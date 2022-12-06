7e90bc959fb1d5fef7dcac96ff9a9b3f88c74b61
Stefano Simoncelli <16114781+s-simoncelli@users.noreply.github.com>
2022-12-06 11:21:07 +0000

    Removed old stub file

--- pywr_editor/utils/Settings.py
+++ pywr_editor/utils/Settings.py
@@ -1,7 +1,7 @@
 import hashlib
 import json
 import os
-from PySide6.QtCore import QSettings
+from PySide6.QtCore import QSettings, QPointF
 from pywr_editor.model import ModelFileInfo
 
 
@@ -52,7 +52,7 @@ class Settings:
         """
         return QSettings(self.org_name)
 
-    def save_window_attributes(self, window):
+    def save_window_attributes(self, window: "MainWindow") -> None:
         """
         Saves the window and widget's geometry and state.
         :param window: The window instance.
@@ -71,7 +71,7 @@ class Settings:
             if hasattr(w, "saveState"):
                 qsettings.setValue(f"{widget}/state", w.saveState())
 
-    def restore_window_attributes(self, window):
+    def restore_window_attributes(self, window: "MainWindow") -> None:
         """
         Restores the  window and widget's geometry and state.
         :param window: The window instance.
@@ -91,7 +91,7 @@ class Settings:
                 w.restoreState(qsettings.value(f"{widget}/state"))
 
     @property
-    def is_schematic_locked(self):
+    def is_schematic_locked(self) -> bool:
         """
         Returns the schematic lock status.
         :return: True if the schematic is locked, False otherwise.
@@ -100,7 +100,7 @@ class Settings:
             self.instance.value(self.lock_key, defaultValue=False)
         )
 
-    def save_schematic_lock(self, lock):
+    def save_schematic_lock(self, lock: bool) -> None:
         """
         Sets the schematic lock status.
         :return: None.
@@ -110,7 +110,7 @@ class Settings:
         self.instance.setValue(self.lock_key, lock)
 
     @property
-    def are_labels_hidden(self):
+    def are_labels_hidden(self) -> bool:
         """
         Whether the schematic labels should be hidden.
         :return: True if the schematic label are hidden, False otherwise.
@@ -119,7 +119,7 @@ class Settings:
             self.instance.value(self.hide_labels_key, defaultValue=False)
         )
 
-    def save_hide_labels(self, hide):
+    def save_hide_labels(self, hide: bool) -> None:
         """
         Sets the display status of the schematic labels.
         :param hide: True to hide the labels, False otherwise.
@@ -130,7 +130,7 @@ class Settings:
         self.instance.setValue(self.hide_labels_key, hide)
 
     @property
-    def are_edge_arrows_hidden(self):
+    def are_edge_arrows_hidden(self) -> bool:
         """
         Whether the schematic edge arrows should be hidden.
         :return: True if the schematic arrows are hidden, False otherwise.
@@ -139,7 +139,7 @@ class Settings:
             self.instance.value(self.hide_arrows_key, defaultValue=False)
         )
 
-    def save_hide_arrows(self, hide):
+    def save_hide_arrows(self, hide: bool) -> None:
         """
         Sets the display status of the schematic edge arrows.
         :param hide: True to hide the edge arrows, False otherwise.
@@ -150,14 +150,14 @@ class Settings:
         self.instance.setValue(self.hide_arrows_key, hide)
 
     @property
-    def zoom_level(self):
+    def zoom_level(self) -> float:
         """
         Returns the schematic zoom level.
         :return: None
         """
         return float(self.instance.value(self.zoom_level_key, defaultValue=1))
 
-    def save_zoom_level(self, zoom_level):
+    def save_zoom_level(self, zoom_level: float) -> None:
         """
         Stores the schematic zoom level.
         :param zoom_level: The zoom level
@@ -168,14 +168,16 @@ class Settings:
         self.instance.setValue(self.zoom_level_key, zoom_level)
 
     @property
-    def schematic_center(self):
+    def schematic_center(self) -> QPointF:
         """
         Returns the schematic centre.
         :return: None
         """
         return self.instance.value(self.schematic_center_key)
 
-    def save_schematic_center(self, position):
+    def save_schematic_center(
+        self, position: [QPointF] | list[QPointF]
+    ) -> None:
         """
         Stores the schematic zoom level.
         :param position: The schematic centre as list or tuple of QPointF.
@@ -185,7 +187,7 @@ class Settings:
             return
         self.instance.setValue(self.schematic_center_key, position)
 
-    def get_recent_files(self):
+    def get_recent_files(self) -> list[dict[str, str | ModelFileInfo]]:
         """
         Returns the list of recent open files.
         :return: The files as dictionary or an empty list if no file has been stored.
@@ -220,7 +222,7 @@ class Settings:
                                 }
                             )
                             valid_files.append(json_file)
-                except Exception as e:
+                except Exception:
                     pass
 
         # store only existing files
@@ -228,7 +230,7 @@ class Settings:
 
         return file_info
 
-    def save_recent_file(self, file):
+    def save_recent_file(self, file: str) -> None:
         """
         Stores a new file in the recent file list.
         :return: None.
@@ -245,7 +247,7 @@ class Settings:
 
         self.global_instance.setValue(self.recent_projects_key, recent_files)
 
-    def clear_recent_files(self):
+    def clear_recent_files(self) -> None:
         """
         Clears the list of recent files.
         :return: None
@@ -253,9 +255,10 @@ class Settings:
         self.global_instance.setValue(self.recent_projects_key, [])
 
     @staticmethod
-    def str_to_bool(value):
+    def str_to_bool(value: str) -> bool:
         """
-        Converts a string to boolean. QSettings stores python boolean as lowercase strings.
+        Converts a string to boolean. QSettings stores python boolean as lowercase
+        strings.
         :param value: The value to convert.
         :return: The string as boolean.
         """
