82b2b4b6a07b1b6fc96c4c1b9681a040e222b497
Stefano Simoncelli <16114781+s-simoncelli@users.noreply.github.com>
2022-12-13 13:50:46 +0000

    Improved typing in utils

--- pywr_editor/utils/__init__.py
+++ pywr_editor/utils/__init__.py
@@ -1,9 +1,10 @@
-from actions import Actions, Action
-from dataframe_utils import *
-from exception_handler import ExceptionHandler
-from helpers import *
-from jump_list import JumpList
-from log import *
-from qt import *
-from settings import Settings
-from strings import *
+from .log import *
+from .exception_handler import ExceptionHandler
+
+from .actions import Actions, Action
+from .dataframe_utils import *
+from .helpers import *
+from .jump_list import JumpList
+from .qt import *
+from .settings import Settings
+from .strings import *
