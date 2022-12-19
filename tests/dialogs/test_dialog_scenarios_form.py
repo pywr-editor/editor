71bf09d2fc4c71cfb8dd332515fd81682befc6a8
Stefano Simoncelli <16114781+s-simoncelli@users.noreply.github.com>
2022-12-19 14:16:09 +0000

    Fixed tests for scenario dialog

--- tests/dialogs/test_dialog_scenarios_form.py
+++ tests/dialogs/test_dialog_scenarios_form.py
@@ -8,7 +8,7 @@ from pywr_editor.dialogs.scenarios.scenario_options_widget import (
 )
 from pywr_editor.form import FormField
 from pywr_editor.widgets import SpinBox
-from tests.utils import resolve_model_path
+from tests.utils import resolve_model_path, change_table_view_cell
 
 
 class TestScenariosDialog:
@@ -351,27 +351,25 @@ class TestScenariosDialog:
         x = table.columnViewportPosition(1) + 5
 
         # Empty name 1
-        y = table.rowViewportPosition(0) + 10
-        qtbot.mouseClick(
-            table.viewport(), Qt.MouseButton.LeftButton, pos=QPoint(x, y)
+        change_table_view_cell(
+            qtbot=qtbot,
+            table=table,
+            row=0,
+            column=1,
+            old_name="First",
+            new_name="",
         )
-        qtbot.mouseDClick(
-            table.viewport(), Qt.MouseButton.LeftButton, pos=QPoint(x, y)
-        )
-        qtbot.keyClick(table.viewport().focusWidget(), Qt.Key_Enter)
-        qtbot.wait(100)
         assert model.names == ["", model.names[1]]
 
         # Empty all
-        y = table.rowViewportPosition(1) + 10
-        qtbot.mouseClick(
-            table.viewport(), Qt.MouseButton.LeftButton, pos=QPoint(x, y)
+        change_table_view_cell(
+            qtbot=qtbot,
+            table=table,
+            row=1,
+            column=1,
+            old_name="Second",
+            new_name="",
         )
-        qtbot.mouseDClick(
-            table.viewport(), Qt.MouseButton.LeftButton, pos=QPoint(x, y)
-        )
-        qtbot.keyClick(table.viewport().focusWidget(), Qt.Key_Enter)
-        qtbot.wait(100)
         assert model.names == ["", ""]
 
         # Fill one name at the time
