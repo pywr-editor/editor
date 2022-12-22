96b007031c45b24967bd06380483ece8aa786179
Stefano Simoncelli <16114781+s-simoncelli@users.noreply.github.com>
2022-12-22 07:29:27 +0000

    Added AddNodeCommand

--- pywr_editor/schematic/__init__.py
+++ pywr_editor/schematic/__init__.py
@@ -3,6 +3,7 @@ from .connecting_node_props import ConnectingNodeProps
 from .schematic_bbox_utils import SchematicBBoxUtils
 from .schematic_node_utils import *
 from .commands.delete_node_command import DeleteNodeCommand
+from .commands.add_node_command import AddNodeCommand
 from .schematic_item import SchematicItem, SchematicLabel
 from .schematic import Schematic
 from .edge import *
