690f0c4b47950abe666d55c55582fa8af49c69a5
Stefano Simonelli <16114781+s-simoncelli@users.noreply.github.com>
2023-03-19 09:16:22 +0000

    Fixed url widget test with excel indexes

--- tests/widgets/test_url_widget.py
+++ tests/widgets/test_url_widget.py
@@ -224,6 +224,11 @@ class TestDialogParameterUrlWidget:
             model_param_dict["start"] = form.find_field_by_name("start").value()
         for f in fields:
             value = form.find_field_by_name(f).widget.get_value()
+            # convert index_col to integer
+            if f == "index_col" and param_name == "param_excel_file":
+                all_cols = list(url_widget.table.columns)
+                value = [all_cols.index(col_name) for col_name in value]
+
             if value:
                 model_param_dict[f] = value
 
