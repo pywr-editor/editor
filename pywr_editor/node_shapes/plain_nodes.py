d29a6d52746a8d523779b4838e4558397e5929fc
Stefano Simonelli <16114781+s-simoncelli@users.noreply.github.com>
2024-02-18 05:44:52 +0000

    Added more plain node colours

--- pywr_editor/node_shapes/plain_nodes.py
+++ pywr_editor/node_shapes/plain_nodes.py
@@ -89,3 +89,159 @@ class PinkCircle(Circle):
             fill=Color("pink", 200),
             outline=Color("pink", 600),
         )
+
+
+class StoneCircle(Circle):
+    def __init__(self, parent: "SchematicNode"):
+        """
+        Initialises the shape for a stone circle.
+        """
+        super().__init__(
+            parent=parent,
+            fill=Color("stone", 200),
+            outline=Color("stone", 600),
+            label=Color("stone", 700),
+        )
+
+
+class AmberCircle(Circle):
+    def __init__(self, parent: "SchematicNode"):
+        """
+        Initialises the shape for a amber circle.
+        """
+        super().__init__(
+            parent=parent,
+            fill=Color("amber", 200),
+            outline=Color("amber", 600),
+            label=Color("amber", 700),
+        )
+
+
+class YellowCircle(Circle):
+    def __init__(self, parent: "SchematicNode"):
+        """
+        Initialises the shape for a yellow circle.
+        """
+        super().__init__(
+            parent=parent,
+            fill=Color("yellow", 200),
+            outline=Color("yellow", 600),
+            label=Color("yellow", 700),
+        )
+
+
+class LimeCircle(Circle):
+    def __init__(self, parent: "SchematicNode"):
+        """
+        Initialises the shape for a lime circle.
+        """
+        super().__init__(
+            parent=parent,
+            fill=Color("lime", 200),
+            outline=Color("lime", 600),
+            label=Color("lime", 700),
+        )
+
+
+class EmeraldCircle(Circle):
+    def __init__(self, parent: "SchematicNode"):
+        """
+        Initialises the shape for a emerald circle.
+        """
+        super().__init__(
+            parent=parent,
+            fill=Color("emerald", 200),
+            outline=Color("emerald", 600),
+            label=Color("emerald", 700),
+        )
+
+
+class TealCircle(Circle):
+    def __init__(self, parent: "SchematicNode"):
+        """
+        Initialises the shape for a teal circle.
+        """
+        super().__init__(
+            parent=parent,
+            fill=Color("teal", 200),
+            outline=Color("teal", 600),
+            label=Color("teal", 700),
+        )
+
+
+class CyanCircle(Circle):
+    def __init__(self, parent: "SchematicNode"):
+        """
+        Initialises the shape for a cyan circle.
+        """
+        super().__init__(
+            parent=parent,
+            fill=Color("cyan", 200),
+            outline=Color("cyan", 600),
+            label=Color("cyan", 700),
+        )
+
+
+class SkyCircle(Circle):
+    def __init__(self, parent: "SchematicNode"):
+        """
+        Initialises the shape for a sky circle.
+        """
+        super().__init__(
+            parent=parent,
+            fill=Color("sky", 200),
+            outline=Color("sky", 600),
+            label=Color("sky", 700),
+        )
+
+
+class IndigoCircle(Circle):
+    def __init__(self, parent: "SchematicNode"):
+        """
+        Initialises the shape for a indigo circle.
+        """
+        super().__init__(
+            parent=parent,
+            fill=Color("indigo", 200),
+            outline=Color("indigo", 600),
+            label=Color("indigo", 700),
+        )
+
+
+class VioletCircle(Circle):
+    def __init__(self, parent: "SchematicNode"):
+        """
+        Initialises the shape for a violet circle.
+        """
+        super().__init__(
+            parent=parent,
+            fill=Color("violet", 200),
+            outline=Color("violet", 600),
+            label=Color("violet", 700),
+        )
+
+
+class FuchsiaCircle(Circle):
+    def __init__(self, parent: "SchematicNode"):
+        """
+        Initialises the shape for a fuchsia circle.
+        """
+        super().__init__(
+            parent=parent,
+            fill=Color("fuchsia", 200),
+            outline=Color("fuchsia", 600),
+            label=Color("fuchsia", 700),
+        )
+
+
+class RoseCircle(Circle):
+    def __init__(self, parent: "SchematicNode"):
+        """
+        Initialises the shape for a rose circle.
+        """
+        super().__init__(
+            parent=parent,
+            fill=Color("rose", 300),
+            outline=Color("rose", 700),
+            label=Color("rose", 700),
+        )
