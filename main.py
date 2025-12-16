798a009f640b3d178d7d573ca8c83d110918a749
Stefano Simonelli <16114781+s-simoncelli@users.noreply.github.com>
2025-12-16 10:06:15 +0000

    Freeze multiprocessing support

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from pywr_editor import app
+import multiprocessing
 
 if __name__ == "__main__":
+    # fix infinite loop - https://github.com/pyinstaller/pyinstaller/wiki/Recipe-Multiprocessing # noqa:E501
+    multiprocessing.freeze_support()
+    multiprocessing.set_start_method("spawn", force=True)
+
+    from pywr_editor import app
+
     app()
